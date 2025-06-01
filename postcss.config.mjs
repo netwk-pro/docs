e86946ccdb5a120c9e1fde94fe5bfeefcd38cea1
SunDevil311 <github@sl.neteng.cc>
2025-06-01 16:16:53 -0700

    Corrected header

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,11 @@
+/* =========================================================================
+postcss.config.mjs
+
+Copyright © 2025 Network Pro Strategies (Network Pro™)
+SPDX-License-Identifier: CC-BY-4.0 OR GPL-3.0-or-later
+This file is part of Network Pro.
+========================================================================= */
+
 export default {
   plugins: {
     autoprefixer: {}, // Enables autoprefixer and uses Browserslist configuration
